4e46a500c3033b7a80435a8c9984887350756337
yuzexia <xiayuze0406@gmail.com>
2023-06-14 22:20:33 +1000

    Finish file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,26 @@ import { CssBaseline } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import { Movies, MovieInformation, Actors, Profile, Navbar } from "./index";
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <Navbar />
-    <main>
-      <Routes>
-        <Route exact path="/" element={<Movies />} />
-        <Route exact path="/movie/:id" element={<MovieInformation />} />
-        <Route exact path="/actors/:id" element={<Actors />} />
-        <Route exact path="/profile/:id" element={<Profile />} />
-      </Routes>
-    </main>
-  </div>
-);
+import useStyles from "./styles";
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles(() => ({
+  root: {
+    display: "flex",
+    height: "100%",
+  },
+  toolbar: {
+    height: "70px",
+  },
+  content: {
+    flexGrow: 1,
+    Padding: "2em",
+  },
+}));
