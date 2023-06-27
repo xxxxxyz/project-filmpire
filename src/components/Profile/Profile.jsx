c9aa05b44dbfc951d4cd57cd30b9c98719110631
yuzexia <xiayuze0406@gmail.com>
2023-06-27 21:29:40 +1000

    Movie Information Part-A

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import { useSelector } from "react-redux";
 import { userSelector } from "../../features/auth";
+import { ExitToApp } from "@mui/icons-material";
+import { Box, Typography, Button } from "@mui/material";
 
 const Profile = () => {
   const { user } = useSelector(userSelector);
-  return <>{user.username}</>;
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
+
+  const favoriteMovies = [];
+
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? (
+        <Typography variant="h5">
+          Add favorites or watchlist some movies to see them here
+        </Typography>
+      ) : (
+        <Box>FAVORITE MOVIES</Box>
+      )}
+    </Box>
+  );
 };
 
 export default Profile;
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -42,7 +42,14 @@ export const tmdbApi = createApi({
         return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
       },
     }),
+
+    //* Get Movie
+    getMovie: builder.query({
+      query: (id) =>
+        `/movie/${id}?append_to_response=vidoes,credits&api_key=${tmdbApiKey}`,
+    }),
   }),
 });
 
-export const { useGetGenresQuery, useGetMoviesQuery } = tmdbApi;
+export const { useGetGenresQuery, useGetMoviesQuery, useGetMovieQuery } =
+  tmdbApi;
